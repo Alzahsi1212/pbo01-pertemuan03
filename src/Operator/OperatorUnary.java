2704d010b49f14df322447f9c2efb8abc52ee496
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-18 17:29:04 +0800

    OperatorUnary

---
+++ src/Operator/OperatorUnary.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;                    // i bernilai 0 ditambahkan i sehingga i=1 (i++)
+        System.out.println(i);  // i = 1
+        i++;                    // i tadi 1 sekarang ditambahkan lagi
+        System.out.println(i);  // i bernilai 2
+        i=i+1;                  // i++ sama dengan i=i+1
+        System.out.println(i);  // i tadi 2 sekarang 3
+    }
+}
