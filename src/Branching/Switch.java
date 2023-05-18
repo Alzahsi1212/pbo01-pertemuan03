d1345658bbebbcea1233a339a13f9a6161f3a0b1
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-18 18:14:42 +0800

    percabangan Switch contoh 2

--- src/Branching/Switch.java
+++ src/Branching/Switch.java
@@ -2,6 +2,7 @@ package Branching;
 
 public class Switch {
     public static void main(String[] args) {
+        char nilai='A';
         int hewan=3;
         //contoh 1
         switch (hewan){
@@ -17,5 +18,24 @@ public class Switch {
             case 4:
                 System.out.println("Kura-Kura");
         }
+        //contoh 2
+        switch (nilai) {
+            case 'A':
+                System.out.println("A  : Selamat atas kemenangan mu ya");
+                System.out.println("B  : Terimakasih ucapannya");
+                break;
+            case 'B':
+                System.out.println("A  : Kawakah minjam dulu seratus hehe");
+                System.out.println("B  : Aduh Masbro..");
+                break;
+            case 'C':
+                System.out.println("Sistem error, tidak ada percakapan");
+                break;
+            default:
+                System.out.println("A  : kada paham aku tugas nya meapa");
+                System.out.println("B  : joki aja kah?");
+                System.out.println("A  : gas nah");
+        }
+
     }
 }
