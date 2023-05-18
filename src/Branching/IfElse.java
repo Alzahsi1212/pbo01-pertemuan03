1bb58d346ceeeee6659fe32f71445b6f0146b12b
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-18 17:57:57 +0800

    percabangan IfElse statement 3

--- src/Branching/IfElse.java
+++ src/Branching/IfElse.java
@@ -3,15 +3,18 @@ package Branching;
 public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double uangDiDompet = 63000;
         double totalBeli = 25000;
         double totalUang = 45000;
 
         //statement 1
         if(uangDiDompet<totalBelanja) {
             System.out.println("Uang kurang, jangan jajan mahal");
+        } else if (uangDiDompet>totalBelanja){
+            double kembalian = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, ini kembaliannya "+kembalian);
         } else {
-            System.out.println("Uang cukup, gas beli");
+            System.out.println("Uangnya pas"); //statement 3
         }
         //statement 2
         if (totalUang<totalBeli){
