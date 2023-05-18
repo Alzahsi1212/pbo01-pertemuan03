8eb2ca9fc1c7bc7151920193ebbe58eef90b82c7
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-18 18:04:03 +0800

    percabangan Switch contoh 1

---
+++ src/Branching/Switch.java
@@ -0,0 +1,21 @@
+package Branching;
+
+public class Switch {
+    public static void main(String[] args) {
+        int hewan=3;
+        //contoh 1
+        switch (hewan){
+            case 1:
+                System.out.println("Ayam");
+                break;
+            case 2:
+                System.out.println("Kucing");
+                break;
+            case 3:
+                System.out.println("Naga");
+                break;
+            case 4:
+                System.out.println("Kura-Kura");
+        }
+    }
+}
