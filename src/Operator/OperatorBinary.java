73602258b6f6fa3f6a93a7a8a715c7ac9aac004c
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-18 17:17:49 +0800

    OperatorBinary assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai penugasan
+    //menempatkan nilai kedalam variable
+    // baris 5-7 merupakan koment yg tidak dijalankan program
+
+    String nama = "M. Alzahsi Ansari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
